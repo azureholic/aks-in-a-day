dd4e261240e06394044ebd512fde4ebefcfb9067
Remco Brosky <remco.brosky@microsoft.com>
2023-05-29 14:08:55 +0200

    container builds

--- prep/acr-build.ps1
+++ prep/acr-build.ps1
@@ -1,23 +1,14 @@
 $currentFolder = Get-Location
 
-#### BUILD THE .NET Framework App ####
-Set-Location ..\src\DotnetFrameworkApp
-
-az acr build -r aksinaday `
-             -t aksinaday.azurecr.io/webapp01:latest `
-             --platform windows `
-             . `
-             --debug
-             
+#### BUILD THE .NET 7 App ####
+Set-Location ..\src\DotNetCoreApp
+#az acr build -r aksinaday -t aksinaday.azurecr.io/webapp02:latest --platform linux .
 Set-Location $currentFolder
 
-#### BUILD THE .NET 7 App ####
-# Set-Location ..\src\DotNetCoreApp
-# az acr build -r aksinaday `
-#              -t aksinaday.azurecr.io/webapp02:latest `
-#              --platform linux `
-#              .
-
+#### BUILD THE .NET Framework App ####
+Set-Location ..\src\DotnetFrameworkApp
+copy Dockerfile ./publish
+az acr build -r aksinaday -t aksinaday.azurecr.io/webapp01:latest --platform windows ./publish
 Set-Location $currentFolder
 
 
